69f3966bf46363072bfa314084f89d374baaf1d8
Julian Guerrero <71576846+jguerrero142@users.noreply.github.com>
2021-09-07 16:10:03 -0500

    sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav
   title = 'sidebar';
+
+  constructor(private observer: BreakpointObserver){}
+
+  ngAfterViewInit(){
+    this.observer.observe(['(max-width: 700px)'])
+    .subscribe((res) => {
+      if(res.matches){
+        this.sidenav.mode = 'over';
+        this.sidenav.close();
+      } else{
+        this.sidenav.mode = 'side';
+        this.sidenav.open();
+      }
+    });
+  }
 }
